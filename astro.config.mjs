b88d526a0a57b9b21d30960ad6191c06ad660540
Gibran Iqbal <gibran.iqbal91@gmail.com>
2025-05-07 12:42:56 -0500

    feat: Set up Astro project with Starlight integration, added configuration files, and initial content structure. Updated .gitignore to include build artifacts and environment files. Removed index.html and LICENSE files. Added package.json and package-lock.json for dependency management.

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightBlog from 'starlight-blog';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'JibbsBlog',
+			plugins: [
+				starlightBlog(),
+			],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Posts',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Rangency', slug: 'posts/rangency' },
+					],
+				},
+			],
+		}),
+	],
+});
