a05880e59768c0f15ea1d743d6513ebedcc9533f
Gibran Iqbal <gibran.iqbal91@gmail.com>
2025-05-25 23:24:49 -0500

    feat: Update Astro configuration for YarpDotCloud, add logo, and enhance social links. Introduce new post on Rangency and remove outdated example reference.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,28 @@ import starlightBlog from 'starlight-blog';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'JibbsBlog',
+			title: 'YarpDotCloud',
 			plugins: [
 				starlightBlog(),
 			],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			site: 'https://www.yarp.cloud',
+			// Add logo
+			logo: {
+				src: './src/assets/yarpdotcloudpokemonlogo.svg',
+			  },
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/jibbscript' },
+				{ icon: 'linkedin', label: 'LinkedIn', href: 'https://linkedin.com/in/gibran.iqbal' },
+				{ icon: 'email', label: 'Email', href: 'mailto:giqbal@gibraniq.com' },
+			],
 			sidebar: [
 				{
 					label: 'Posts',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Rangency', slug: 'posts/rangency' },
+						{ label: 'FedRAMP Data Mesh Reference Architecture', slug: 'posts/fedramp-datamesh' },
+						{ label: 'YarpDotCloud', slug: 'posts/yarpdotcloud' },
 					],
 				},
 			],
@@ -25,4 +36,3 @@ export default defineConfig({
 		}),
 	],
 });
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
---
+++ src/content/docs/posts/rangency.md
@@ -0,0 +1,14 @@
+---
+title: Rangency 
+description: Range + Agency in the Age of AI
+---
+
+
+
+## Abstract
+
+The Age of AI is upon us. The world is changing, and so are the roles and responsibilities of those who live and work in it. This article explores the concept of Rangency, which is the combination of Range and Agency. It delves into the implications of Rangency in the Age of AI, and how it can be used to guide our actions and decisions.
+
+## Introduction
+
+The Age of AI is upon us. The world is changing, and so are the roles and responsibilities of those who live and work in it. This article explores the concept of Rangency, which is the combination of Range and Agency.
